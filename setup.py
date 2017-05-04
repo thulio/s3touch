d53ff5c400c6f06f525f929f100ab5c3978718c3
Thúlio Costa <thulio@thuliocosta.com>
2017-05-03 22:32:39 -0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import codecs
 import os.path
-from setuptools import setup, find_packages
+from setuptools import setup
 
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -10,16 +10,9 @@ with codecs.open(os.path.join(PROJECT_DIR, 'README.rst'), encoding='utf-8') as f
 
 setup(
     name='s3touch',
-    version='0.1.1',
-    packages=find_packages(exclude=('tests', 'tests.*')),
-    install_requires=[
-        'awscli==1.11.82',
-        'click==6.7',
-    ],
+    version='0.1.2',
+    py_modules=['cli'],
     entry_points={"console_scripts": ["s3touch = cli:main"]},
-    test_suite='tests',
-    tests_require=[
-    ],
     url='https://github.com/thulio/s3touch',
     license='MIT',
     author='Thúlio Costa',
